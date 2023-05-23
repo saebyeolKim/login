4bb782a1b20d8db4c3dd15f4f7d267a08d34f9b5
saebyeolKim <toquf2611@naver.com>
2023-05-23 22:57:03 +0900

    로그인 처리2 - 서블릿 필터 - 인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +18,14 @@ public class WebConfig {
         filterFilterRegistrationBean.addUrlPatterns("/*"); //모든 url 에 적용
 
         return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 }
